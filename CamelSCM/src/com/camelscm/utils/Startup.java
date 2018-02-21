24234792a83e6e1531485dc0843347613dc48ceb
Salim Said Hemed <salimsaidhemed1989@gmail.com>
2018-02-21 19:32:01 +0300

    Committed by Salim Said Hemed at 2018-02-21 19:32:01

--- CamelSCM/src/com/camelscm/utils/Startup.java
+++ CamelSCM/src/com/camelscm/utils/Startup.java
@@ -5,10 +5,47 @@
  */
 package com.camelscm.utils;
 
+import java.io.File;
+
 /**
  *
- * @author salim
+ * @author Salim Said Hemed
+ * @version 0.1
+ * @description Program Startup Procedures:
+ * <h1>Order of Initialization</h1>
+ * <li>
+ * <ol></ol>
+ * </li>
+ * 
  */
 public class Startup {
-    
+    /**
+     * <h1>Checks if a Database exists.</h1> 
+     * Can be used to determine if the database, 
+     * if not it can be created by the system
+     * <h2>Usage : </h2>
+     * <code>if (databaseExists()){} </code>
+     * @return boolean 
+     * 
+     */
+    private boolean databaseExists(){
+        File file = new File("Database");
+        if (!file.exists() || !file.isDirectory()){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    /**
+     * 
+     */
+    public void startupProcedures(){
+        if (!databaseExists()){
+        
+        }
+        else {
+        
+        }
+    }
 }
